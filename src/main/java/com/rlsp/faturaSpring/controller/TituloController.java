6727025934e0febace4f0b02d4c0f9c5480c5435
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-05-03 14:54:43 -0300

    Errors thymeleaf

--- src/main/java/com/rlsp/faturaSpring/controller/TituloController.java
+++ src/main/java/com/rlsp/faturaSpring/controller/TituloController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,23 +27,24 @@ public class TituloController {
 	public ModelAndView novo() {
 	
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		
+		mv.addObject(new Titulo());
 		return mv; // Nome da VIEW sem .html
-	}
-	
-	
-	
+	}	
 	
 	/**
 	 * ModelAndView ==> retorma uma VIEW e mais INFORMACOES junto 
 	 * 
 	 */
-	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo) {
+	@RequestMapping(method = RequestMethod.POST)	
+	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		if (errors.hasFieldErrors()) {
+			return mv;
+		}
+		
 		titulos.save(titulo);
 		
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("mensagem", "Titulo salvo com sucesso");
+		mv.addObject("mensagem", "Título salvo com sucesso!");
 		return mv;
 	}
 	
@@ -54,7 +57,10 @@ public class TituloController {
 	}
 	
 	@RequestMapping
-	public String pesquisar() {
-		return "PesquisaTitulo";
+	public ModelAndView pesquisar() {
+		List<Titulo> todosTitulos = titulos.findAll();
+		ModelAndView mv = new ModelAndView("PesquisaTitulo");
+		mv.addObject("titulos", todosTitulos);
+		return mv;
 	}
 }
--- src/main/java/com/rlsp/faturaSpring/model/Titulo.java
+++ src/main/java/com/rlsp/faturaSpring/model/Titulo.java
@@ -11,9 +11,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
+
 
 @Entity
 public class Titulo {
@@ -22,17 +29,35 @@ public class Titulo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@NotBlank(message = "Descrição é obrigatória")
+	@Size(max = 60, message = "A descrição não pode conter mais de 60 caracteres")
 	private String descricao;
 	
+	@NotNull(message = "Date de vencimento é obrigatória")
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 	
+	@NotNull(message = "Valor não pode ser vazio")
+	@DecimalMin(value = "0.01", message = "Valor não pode ser menor que 0,01")
+	@DecimalMax(value = "9999999.99", message = "Valor não pode ser maior que 9.999.999,99")
 	@NumberFormat(pattern = "#,###,##0.00")
 	private BigDecimal valor;
 	
 	@Enumerated(EnumType.STRING)
 	private StatusTitulo status;
+	
+	public boolean isPendente() {
+		return StatusTitulo.PENDENTE.equals(this.status);
+	}
+	
+	public boolean isRecebidoComMulta() {
+		return StatusTitulo.RECEBIDOCOMULTA.equals(this.status);
+	}
+	
+	public boolean isRecebido() {
+		return StatusTitulo.RECEBIDO.equals(this.status);
+	}
 
 	public Long getCodigo() {
 		return codigo;
@@ -98,5 +123,7 @@ public class Titulo {
 			return false;
 		return true;
 	}
+
+
 
 }
